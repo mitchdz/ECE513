88fb5cc2f69f9a47aed2d6096d0934a4f6177569
mitchdz <mitch_dz@hotmail.com>
2019-10-21 22:51:50 +0000

    in class register.js

--- public/holz/javascripts/register.js
+++ public/holz/javascripts/register.js
@@ -1,13 +1,41 @@
 function sendRegisterRequest() {
-  // TODO
+  let email = $('#email');
+  let passowrd = $'#password').val();
+  let fullName = $('#fullName').val();
+  let passwordConfirm = $('#passwordConfirm').val();
+  
+  if (password != passwordConfirm) {
+    $('#ServerResponse').html('<p>Password does not match.</p>');
+    $('#ServerResponse').show();
+    return;
+  }
+  
+  $.ajax({
+    url:	'/users/register'.
+    type:	'POST',
+    contentType:	'application/json',
+    data:		JSON.stringify({email:email, fullName:fullName, password:password}),
+    dataType:	'JSON'
+  })
+    .done(registerSuccess)
+    .fail(registerError)
 }
 
 function registerSuccess(data, textSatus, jqXHR) {
-  // TODO
+  if (data.success) {
+    window.location = "index.html";
+  }
 }
 
 function registerError(jqXHR, textStatus, errorThrown) {
-  // TODO
+  if (jqXHR.statusCode == 404) {
+    $('ServerResponse').html('<p>Server could not be reached.</p>');
+    $('ServerResponse').show();
+  }
+  else {
+    $('ServerResponse').html('<[>Error: ' + jqXHR.responseJSON.message + '</p>');
+    $('ServerResponse').show();
+  }
 }
 
 $(function () {
