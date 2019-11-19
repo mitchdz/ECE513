8850b4112f3b0d9a7a934c5d16d482e0421024d0
mitchdz <mitch_dz@hotmail.com>
2019-11-19 22:44:47 +0000

    deviceData: make gps_long and gps_lat required

--- models/deviceData.js
+++ models/deviceData.js
@@ -2,10 +2,10 @@ var db = require("../devicedb");
 
 var deviceDataSchema = new db.Schema({
     gps_exists: { type: boolean, required: true },
-    gps_lat:    Number,
-    gps_long:   Number,
-    uv:         { type: Number, required: true},
-    time:       { type: Date, required: true},
+    gps_lat:    { type: Number, required: true },
+    gps_long:   { type: Number, required: true },
+    uv:         { type: Number, required: true },
+    time:       { type: Date, required: true },
     deviceId:   { type: String, required: true },
     APIkey:     { type: String, required: true },
 });
